a0fbc62a52189fdaff58bc5521c76a7d0823b422
dubudab <59426272+dubudab@users.noreply.github.com>
2020-01-16 02:07:09 -0500

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,7 @@ $message = $_POST['message'];
 $to = 'commissions@chefshire.page'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Inquiry:  $name";
 $email_body = "You have received a new message from your website inquiry form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@chefshire.page\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: inquiry@chefshire.page\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;		
